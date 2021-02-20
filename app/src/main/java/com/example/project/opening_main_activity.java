322735a10778d1e542bfa704b07c077ad04a094d
Igor <77459655+success-412@users.noreply.github.com>
2021-02-19 20:00:36 -0800

    added back button on opening page

--- app/src/main/java/com/example/project/opening_main_activity.java
+++ app/src/main/java/com/example/project/opening_main_activity.java
@@ -2,7 +2,9 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class opening_main_activity extends AppCompatActivity {
 
@@ -11,4 +13,10 @@ public class opening_main_activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_opening_main_activity);
     }
+
+    public void onClick_opennings_back1(View view) {
+        Intent openings_intent_back1 = new Intent(this, MainActivity.class);
+        startActivity(openings_intent_back1);
+    }
+
 }
