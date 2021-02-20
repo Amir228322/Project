662f7880cf996d49f4ae4eaeb53ae82a5513ac08
Igor <77459655+success-412@users.noreply.github.com>
2021-02-19 21:01:38 -0800

    added back buttons for activities

--- app/src/main/java/com/example/project/italian_opening.java
+++ app/src/main/java/com/example/project/italian_opening.java
@@ -2,7 +2,9 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class italian_opening extends AppCompatActivity {
 
@@ -11,4 +13,10 @@ public class italian_opening extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_italian_opening);
     }
+
+    public void onClick_italian_back(View view) {
+        Intent italian_intent_back = new Intent(this, opening_main_activity.class);
+        startActivity(italian_intent_back);
+    }
+
 }
--- app/src/main/java/com/example/project/sicilian_opening.java
+++ app/src/main/java/com/example/project/sicilian_opening.java
@@ -2,7 +2,9 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class sicilian_opening extends AppCompatActivity {
 
@@ -11,4 +13,10 @@ public class sicilian_opening extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sicilian_opening);
     }
+
+    public void onClick_sicilian_back(View view) {
+        Intent sicilian_intent_back = new Intent(this, opening_main_activity.class);
+        startActivity(sicilian_intent_back);
+    }
+
 }
