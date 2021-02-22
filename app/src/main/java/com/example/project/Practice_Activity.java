0ecd10adee84295c03c05e3f0574bfc00112daed
ShahbekMiru <60500115+shahbek@users.noreply.github.com>
2021-02-22 15:46:17 -0800

    Practicing openings screen

--- app/src/main/java/com/example/project/Practice_Activity.java
+++ app/src/main/java/com/example/project/Practice_Activity.java
@@ -2,7 +2,9 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Practice_Activity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class Practice_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practice_);
+    }
+    public void onClick_opennings_back2(View view) {
+        Intent openings_intent_back2 = new Intent(this, MainActivity.class);
+        startActivity(openings_intent_back2);
     }
 }
