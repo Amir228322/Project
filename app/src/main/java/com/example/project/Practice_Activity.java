6245f67715adb0818210525f5da3db451dbfe626
success-412 <77459655+success-412@users.noreply.github.com>
2021-03-24 17:22:11 -0700

    added sicilian defence: dragon defence and closed defence

--- app/src/main/java/com/example/project/Practice_Activity.java
+++ app/src/main/java/com/example/project/Practice_Activity.java
@@ -12,6 +12,7 @@ public class Practice_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practice);
+        getSupportActionBar().hide();
     }
 
     public void onClick_opennings_back1(View view) {
--- app/src/main/java/com/example/project/opening_main_activity.java
+++ app/src/main/java/com/example/project/opening_main_activity.java
@@ -12,6 +12,7 @@ public class opening_main_activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_opening_main_activity);
+        getSupportActionBar().hide();
     }
 
     public void onClick_opennings_back1(View view) {
